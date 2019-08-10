4879b914a4902ae3530e5a3dc7e4718af583418c
Adrienne Smith <agsmith046@gmail.com>
2019-08-10 17:13:38 -0500

    fixed issue with database

--- migrations/004.do.create.table_relationships.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE packing_list
-  ADD COLUMN
-    user_id INTEGER REFERENCES users(id) ON DELETE SET NULL;
-
-ALTER TABLE transportation
-  ADD COLUMN
-    user_id INTEGER REFERENCES users(id) ON DELETE SET NULL;
--- migrations/004.undo.create.table_relationships.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE packing_list
-  DROP COLUMN user_id
-
-ALTER TABLE transportation
-  DROP COLUMN user_id 
