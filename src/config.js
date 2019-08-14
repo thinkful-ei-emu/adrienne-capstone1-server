681a60cb4a6ea5da36d6fdeb7e991f2713c9e2a4
Adrienne Smith <agsmith046@gmail.com>
2019-08-14 14:37:53 -0500

    changed config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://travel_companion@localhost/travel_companion',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://travel_companion@localhost/travel_companion',
   JWT_SECRET: process.env.JWT_SECRET ||'wogi923utpg[jap4hu2i'
 };
